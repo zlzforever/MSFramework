42f0e37859d691f5681819fb03224cbc0f4e955e
admin <admin@DESKTOP-AEQD6I9>
2019-05-31 16:04:50 +0800

    update rabbitmq

--- src/MSFramework.EventBus.RabbitMQ/ServiceCollectionExtensions.cs
+++ src/MSFramework.EventBus.RabbitMQ/ServiceCollectionExtensions.cs
@@ -6,16 +6,23 @@ namespace MSFramework.EventBus.RabbitMQ
 {
 	public static class ServiceCollectionExtensions
 	{
+		public static MSFrameworkBuilder UseRabbitMQEventBus(this MSFrameworkBuilder builder)
+		{
+			builder.Services.AddRabbitMQEventBus();
+			return builder;
+		}
+
 		public static IServiceCollection AddRabbitMQEventBus(this IServiceCollection services)
-		{			
-			services.AddScoped<RabbitMQOptions>();
+		{
+			services.AddSingleton<RabbitMQOptions>();
 			services.AddSingleton(provider =>
 			{
 				var options = provider.GetRequiredService<RabbitMQOptions>();
 
 				var factory = new ConnectionFactory
 				{
-					HostName = options.ConnectionString,
+					HostName = options.HostName,
+					Port = options.Port,
 					DispatchConsumersAsync = true
 				};
 
@@ -28,7 +35,6 @@ namespace MSFramework.EventBus.RabbitMQ
 				{
 					factory.Password = options.Password;
 				}
-
 				return new RabbitMQConnection(factory,
 					provider.GetRequiredService<ILogger<RabbitMQConnection>>(),
 					options.RetryCount);
