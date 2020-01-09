0b7bede53aa12ffecee98755cc382a03ec53d731
Lewis Zou <zlzforever@163.com>
2020-01-09 13:26:50 +0800

    ignore null configuration

--- src/MSFramework/Extensions/ConfigurationExtensions.cs
+++ src/MSFramework/Extensions/ConfigurationExtensions.cs
@@ -1,7 +1,22 @@
+using System;
+using Microsoft.Extensions.Configuration;
+
 namespace MSFramework.Extensions
 {
-	public class ConfigurationExtensions
+	public static class ConfigurationExtensions
 	{
-		
+		public static void Print(this IConfiguration configuration)
+		{
+			if (configuration == null)
+			{
+				return;
+			}
+
+			Console.WriteLine("Configuration: ");
+			foreach (var kv in configuration.GetChildren())
+			{
+				Console.WriteLine($"{kv.Key} = {kv.Value}");
+			}
+		}
 	}
 }
