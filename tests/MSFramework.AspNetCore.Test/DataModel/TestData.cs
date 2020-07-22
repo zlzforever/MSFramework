653b00a9f634e1d9d270c1824e8902dd7851b1c0
jonechenug <jonechenug@gmail.com>
2020-07-22 22:25:26 +0800

    add test

---
+++ tests/MSFramework.AspNetCore.Test/DataModel/TestData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MSFramework.Common;
+using MSFramework.Domain.AggregateRoot;
+
+namespace MSFramework.AspNetCore.Test.DataModel
+{
+	public class TestData: ModificationAuditedAggregateRoot
+	{
+		public TestData() : base(CombGuid.NewGuid())
+		{
+		}
+	}
+}
---
+++ tests/MSFramework.AspNetCore.Test/EfPostgreSqlTest/EntityConfiguration/TestDataConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MSFramework.AspNetCore.Test.DataModel;
+using MSFramework.AspNetCore.Test.EfPostgreSqlTest.Infrastructure;
+using MSFramework.Ef;
+
+namespace MSFramework.AspNetCore.Test.EfPostgreSqlTest.EntityConfiguration
+{
+	public class TestDataConfiguration: EntityTypeConfigurationBase<TestData, TestDataContext>
+	{
+	}
+}
