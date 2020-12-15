8d3061220c5cb4355877973920a2e904378c34b1
邹嵩 <zlzforever@163.com>
2020-12-15 08:22:46 +0800

    update

--- src/MSFramework/Extensions/StringExtensions.cs
+++ src/MSFramework/Extensions/StringExtensions.cs
@@ -54,5 +54,15 @@ namespace MicroserviceFramework.Extensions
 
 			return index;
 		}
+
+		public static bool IsNullOrEmpty(this string value)
+		{
+			return string.IsNullOrEmpty(value);
+		}
+
+		public static bool IsNullOrWhiteSpace(this string value)
+		{
+			return string.IsNullOrWhiteSpace(value);
+		}
 	}
 }
